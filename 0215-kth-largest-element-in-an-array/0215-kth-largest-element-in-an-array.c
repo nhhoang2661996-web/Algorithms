270f0ddf8914b77f9f0e0bf2d0474eec24a867a2
Nguyen Huy Hoang <nhhoang2661996@gmail.com>
2026-06-13 21:09:11 +0700

    Time: 14 ms (71.64%), Space: 15.4 MB (57.54%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.c
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.c
@@ -1,36 +1,37 @@
-void swap(int* a, int* b) {
-    int t = *a;
+void swap (int* a, int* b ){
+    int n = *a;
     *a = *b;
-    *b = t;
+    *b = n ;
 }
-void minHeap(int* arr, int size, int idx) {
-    int smallest = idx;
-    int left = 2 * idx + 1;
-    int right = 2 * idx + 2;
-
-    if (left < size && arr[left] < arr[smallest])
-        smallest = left;
-    if (right < size && arr[right] < arr[smallest])
-        smallest = right;
-
-    if (smallest != idx) {
-        swap(&arr[idx], &arr[smallest]);
-        minHeap(arr, size, smallest);
+void swiftDown(int* heap, int heapSize,int index){
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int smallest = index;
+    if(right < heapSize &&heap[smallest] > heap[right])smallest = right;
+    if(left < heapSize &&heap[smallest] > heap[left])smallest = left;
+    if(smallest != index){
+        swap(&heap[index],&heap[smallest]);
+        swiftDown(heap,heapSize,smallest);
+    }
+}
+void swiftUp(int* heap, int index) {
+    if (index == 0) return;
+    int parent = (index - 1)/2;
+    if (heap[index] < heap[parent]) {
+        swap(&heap[index],&heap[parent]);
+        swiftUp(heap, parent);
     }
 }
 int findKthLargest(int* nums, int numsSize, int k) {
-    int arr[k];
-    for (int i = 0; i < k; i++) {
-        arr[i] = nums[i];
-    }
-    for (int i = (k / 2) - 1; i >= 0; i--) {
-        minHeap(arr, k, i);
-    }
-    for (int i = k; i < numsSize; i++) {
-        if (nums[i] > arr[0]) {
-            arr[0] = nums[i]; 
-            minHeap(arr, k, 0);
+    int minHeap[k];
+    for(int i = 0; i< numsSize; i ++){
+        if (i < k){
+            minHeap[i] = nums[i];
+            swiftUp(minHeap,i);
+        }else  if (minHeap[0] < nums[i]){
+            minHeap[0] = nums[i];
+            swiftDown(minHeap,k,0);
         }
     }
-    return arr[0];
+    return minHeap[0];
 }
